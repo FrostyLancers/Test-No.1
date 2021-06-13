1d7b8b03fde58eb569cba2d878be441af2e8cee0
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2021-06-13 16:53:02 +0700

    63120501056 Test No.1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Test_No._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 3;
+            int column = 3;
+            int[,] Number = new int[row, column];
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Number[j, i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int NewNumber = int.Parse(Console.ReadLine());
+
+            for (int a = 0; a < 3; a++)
+            {
+                for (int b = 0; b < 3; b++)
+                {
+                    Console.Write(Number[a, b]);
+                    Console.Write("");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
